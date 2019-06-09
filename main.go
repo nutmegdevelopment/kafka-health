61049ea4f35493764c468851b654d5a054a10009
chris-vest <hotdogsandfrenchfries@gmail.com>
2019-06-09 21:03:02 +0100

    Use goroutines & waitgroups for both produce & consume

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	kafka "github.com/segmentio/kafka-go"
@@ -41,14 +42,23 @@ func main() {
 		return
 	}
 
-	go consume(kafkaURL, topic)
+	var wg sync.WaitGroup
+	wg.Add(2)
 
+	go consume(&wg, kafkaURL, topic)
+	go produce(&wg, kafkaURL, topic)
+
+	wg.Wait()
+	log.Println("All done.")
+}
+
+func produce(wg *sync.WaitGroup, kafkaURL string, topic string) {
+	defer wg.Done()
 	writer := newKafkaWriter(kafkaURL, topic)
 	// close writer upon exit
 	defer writer.Close()
-	log.Println("Starting producing ...")
+	log.Println("PRODUCER: Starting producing ...")
 	for i := 0; i < 1; i++ {
-		log.Print("Sleeping...")
 		dt := time.Now()
 		msg := kafka.Message{
 			Key:   []byte(fmt.Sprintf("Key-%d", i)),
@@ -56,21 +66,20 @@ func main() {
 		}
 		err := writer.WriteMessages(context.Background(), msg)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln("PRODUCER:", err)
 		} else {
-			log.Println("Key:", string(msg.Key))
-			log.Println("Value:", string(msg.Value))
-		}
-		for i := 0; i < 15; i++ {
-			log.Printf("Waiting for consumer? %v", i)
-			time.Sleep(1 * time.Second)
+			log.Println("PRODUCER: Key:", string(msg.Key))
+			log.Println("PRODUCER: Value:", string(msg.Value))
 		}
 	}
 }
 
-func consume(kafkaURL string, topic string) {
+func consume(wg *sync.WaitGroup, kafkaURL string, topic string) {
+	// Need to call this explicitly because of the for loop...
+	// Better way?
+	// defer wg.Done()
 	// Consume messages
-	log.Println("Starting consuming ...")
+	log.Println("CONSUMER: Starting consuming ...")
 	// Configure reader
 	rConf := kafka.ReaderConfig{
 		Brokers:   []string{kafkaURL},
@@ -91,11 +100,12 @@ func consume(kafkaURL string, topic string) {
 		m, err := reader.ReadMessage(context.Background())
 		if err != nil {
 			log.Fatalln(err)
+			wg.Done()
 		} else {
 			log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
-			log.Printf("success pew pew")
 			log.Printf("Closing reader")
 			defer reader.Close()
+			wg.Done()
 		}
 	}
 }
